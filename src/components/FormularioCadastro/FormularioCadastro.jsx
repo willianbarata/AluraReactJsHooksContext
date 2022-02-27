1b77c65c0e8d33c1648965389f37cd0039a82e78
willianbarata <willianbarata@gmail.com>
2022-02-27 13:37:14 -0300

    Aula 4 - refatorando switchCase para array

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -8,30 +8,21 @@ function FormularioCadastro({aoEnviar, validarCPF}) {
   
   const [etapaAtual, setEtapaAtual] = useState(0);
 
+  const formularios = [<DadosUsuario aoEnviar={proximo}/>,
+    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF}/>,
+    <DadosEntrega aoEnviar={aoEnviar} />,
+    <Typography> Error ao selecionar formulário </Typography>
+  ]
+
   function proximo(){
     setEtapaAtual(etapaAtual +1);
   }
 
-  function formularioAtual(etapa){
-    switch(etapa){
-      case 0 : 
-        return <DadosUsuario aoEnviar={proximo}/>
-      case 1 :
-        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF}/>
-      case 2 :
-        return <DadosEntrega aoEnviar={aoEnviar} />
-      default:
-        return <Typography> Error ao selecionar formulário </Typography>
-    }
-  }
-
   return (
     <>
-      {formularioAtual(etapaAtual)}
+      {formularios[etapaAtual]}
     </>
     );
 }
-
-
 
 export default FormularioCadastro;
