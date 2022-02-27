0f35db5d7a34997e2b39904924ae029bd606d23a
willianbarata <willianbarata@gmail.com>
2022-02-27 13:17:19 -0300

    Aula 3 - switch de renderização formulário

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,3 +1,4 @@
+import { Typography } from "@material-ui/core";
 import React, { useState } from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
@@ -5,13 +6,28 @@ import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({aoEnviar, validarCPF}) {
   
+  const [etapaAtual, setEtapaAtual] = useState(0);
+
+  function formularioAtual(etapa){
+    switch(etapa){
+      case 0 : 
+        return <DadosUsuario />
+      case 1 :
+        return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
+      case 2 :
+        return <DadosEntrega />
+      default:
+        return <Typography> Error ao selecionar formulário </Typography>
+    }
+  }
+
   return (
     <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-      <DadosUsuario />
-      <DadosEntrega/>
+      {formularioAtual(etapaAtual)}
     </>
     );
 }
+
+
 
 export default FormularioCadastro;
