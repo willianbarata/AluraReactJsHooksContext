aea6e43c1e035a355910dc33758396b0537ab573
willianbarata <willianbarata@gmail.com>
2022-02-27 14:02:09 -0300

    Aula 5 - Criando useState dos campos do form

--- src/components/FormularioCadastro/DadosEntrega.jsx
+++ src/components/FormularioCadastro/DadosEntrega.jsx
@@ -1,10 +1,23 @@
 import { TextField,Button } from "@material-ui/core";
 import React from "react";
+import { useState } from "react";
 
-function DadosEntrega(){
+function DadosEntrega({aoEnviar}){
+
+    const [cep, setCep] = useState("");
+    const [endereco, setEndereco] = useState("");
+    const [numero, setNumero] = useState("");
+    const [estado, setEstado] = useState("");
+    const [cidade, setCidade] = useState("");
+
     return(
-        <form>
+        <form onSubmit={(event)=>{
+            event.preventDefault();
+            aoEnviar({cep,endereco,numero,estado,cidade})
+        }}>
             <TextField
+                value={cep}
+                onChange={(event) => {setCep(event.target.value)}}
                 id="cep"
                 label="CEP"
                 type="number"
@@ -13,6 +26,8 @@ function DadosEntrega(){
                 margin="normal"
             />
             <TextField
+                value={endereco}
+                onChange={(event) => {setEndereco(event.target.value)}}
                 id="endereco"
                 label="Endereço"
                 type="text"
@@ -22,6 +37,8 @@ function DadosEntrega(){
                 fullWidth
             />
             <TextField
+                value={numero}
+                onChange={(event) => {setNumero(event.target.value)}}
                 id="numero"
                 label="Número"
                 type="number"
@@ -30,6 +47,8 @@ function DadosEntrega(){
                 margin="normal"
             />
             <TextField
+                value={estado}
+                onChange={(event) => {setEstado(event.target.value)}}
                 id="estado"
                 label="Estado"
                 type="text"
@@ -38,6 +57,8 @@ function DadosEntrega(){
                 margin="normal"
             />
             <TextField
+                value={cidade}
+                onChange={(event) => {setCidade(event.target.value)}}
                 id="cidade"
                 label="Cidade"
                 type="text"
--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,19 +1,42 @@
 import { TextField, Button } from "@material-ui/core";
-import React from "react";
+import React, {useState} from "react";
 
 function DadosUsuario({aoEnviar}){
+
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
     return(
         <form onSubmit={(event)=>{
             event.preventDefault();
-            aoEnviar();
+            aoEnviar({email, senha});
         }}>
-            <TextField id="email" label="E-Mail" type="email" required variant="outlined"
-        margin="normal"
-        fullWidth/>
-            <TextField id="senha" label="Senha" type="password" required variant="outlined"
-        margin="normal"
-        fullWidth/>
-            <Button type="submit" variant="contained" color="primary"> Cadastrar</Button>
+            <TextField 
+                value={email}
+                onChange={(event) => {setEmail(event.target.value)}}
+                id="email" 
+                label="E-Mail" 
+                type="email"
+                required 
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+            <TextField 
+                value={senha}
+                onChange={(event) => {setSenha(event.target.value)}}
+                id="senha" 
+                label="Senha" 
+                type="password" 
+                required 
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+            <Button 
+                type="submit" 
+                variant="contained" 
+                color="primary"> Cadastrar</Button>
         </form>
     );
 }
